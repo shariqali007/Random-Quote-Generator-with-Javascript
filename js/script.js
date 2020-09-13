a7545e700ce3734ef37ff15519b1d6d3ccecafe4
ali <ali_shariq@icloud.com>
2020-09-13 15:15:16 +0500

    quotes added

--- js/script.js
+++ js/script.js
@@ -11,12 +11,69 @@ project 1 - A Random Quote Generator
  * `quotes` array 
 ***/
 
-
+const quotes = [
+  {
+    quote: 'Nothing vast enter the life of mortals without a curse.',
+    source: 'Sophocles'
+  },
+  {
+    quote: "There are only two industries that call thier customers 'users': illegal drugs and software.",
+    source: 'Edward Tufte'
+  },
+  {
+    quote: 'Any Sufficiently advanced technology is indistinguishable from magic.',
+    source: 'Arthur C. Clarke'
+  },
+  {
+    quote: 'Optimism is the faith that leads to achievement.',
+    source: 'Helen Keller'
+  },
+  {
+    quote: 'Art, freedom and creativity will change society faster than politics.',
+    source: 'Victor Pinchuk'
+  },
+  {
+    quote: 'You can do anything but not everything',
+    source: 'David Allen'
+  },
+  {
+    quote: 'Make it so!',
+    source: 'Captain Jean Luc Picard',
+    citation: 'Star Trek: The Next Generation'
+  },
+  {
+    quote: 'Humility is no substitute for a good personality.',
+    source: 'Fran Leibowitz',
+    citation: 'Metropolitan Life',
+    year: '1978'
+  },
+  {
+    quote: 'You have your moments. Not many of them, but you do have them.',
+    source: 'Princess Leia',
+    citation: 'The Empire Strikes Back',
+    year: '1980'
+  }
+]
 
 /***
  * `getRandomQuote` function
 ***/
 
+/**
+ * Returns a random quote from quotes array using randomNum.
+ *
+ * @param {number} array - The highest number value.
+ * @return {string} The random Quote.
+ */
+
+function getRandomQuote(array) {
+  let randomQuote = [];
+  let randomNum = Math.floor(Math.random() * array.length);
+  if (array.indexOf() === randomNum) {
+    randomQuote.push(array.quote);
+  }
+  return randomQuote;
+}
 
 
 /***
