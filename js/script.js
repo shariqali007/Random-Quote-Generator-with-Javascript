d8d5b65886aba8724bc0175075d5fc88a49207f6
ali <ali_shariq@icloud.com>
2020-09-14 16:24:36 +0500

    update script.js

--- js/script.js
+++ js/script.js
@@ -4,103 +4,107 @@ project 1 - A Random Quote Generator
 ******************************************/
 
 // For assistance: 
-  // Check the "Project Resources" section of the project instructions
-  // Reach out in your Slack community - https://treehouse-fsjs-102.slack.com/app_redirect?channel=chit-chat
+// Check the "Project Resources" section of the project instructions
+// Reach out in your Slack community - https://treehouse-fsjs-102.slack.com/app_redirect?channel=chit-chat
 
 /*** 
  * `quotes` array 
-***/
+ ***/
 
-const quotes = [
-  {
-    quote: 'Nothing vast enter the life of mortals without a curse.',
-    source: 'Sophocles'
-  },
-  {
-    quote: "There are only two industries that call thier customers 'users': illegal drugs and software.",
-    source: 'Edward Tufte'
-  },
-  {
-    quote: 'Any Sufficiently advanced technology is indistinguishable from magic.',
-    source: 'Arthur C. Clarke'
-  },
-  {
-    quote: 'Optimism is the faith that leads to achievement.',
-    source: 'Helen Keller'
-  },
-  {
-    quote: 'Art, freedom and creativity will change society faster than politics.',
-    source: 'Victor Pinchuk'
-  },
-  {
-    quote: 'You can do anything but not everything',
-    source: 'David Allen'
-  },
-  {
-    quote: 'Make it so!',
-    source: 'Captain Jean Luc Picard',
-    citation: 'Star Trek: The Next Generation'
-  },
-  {
-    quote: 'Humility is no substitute for a good personality.',
-    source: 'Fran Leibowitz',
-    citation: 'Metropolitan Life',
-    year: '1978'
-  },
-  {
-    quote: 'You have your moments. Not many of them, but you do have them.',
-    source: 'Princess Leia',
-    citation: 'The Empire Strikes Back',
-    year: '1980'
-  }
+const quotes = [ {
+  quote: 'Nothing vast enter the life of mortals without a curse.',
+  source: 'Sophocles'
+},
+{
+  quote: "There are only two industries that call thier customers 'users': illegal drugs and software.",
+  source: 'Edward Tufte'
+},
+{
+  quote: 'Any Sufficiently advanced technology is indistinguishable from magic.',
+  source: 'Arthur C. Clarke'
+},
+{
+  quote: 'Optimism is the faith that leads to achievement.',
+  source: 'Helen Keller'
+},
+{
+  quote: 'Art, freedom and creativity will change society faster than politics.',
+  source: 'Victor Pinchuk'
+},
+{
+  quote: 'You can do anything but not everything',
+  source: 'David Allen'
+},
+{
+  quote: 'Make it so!',
+  source: 'Captain Jean Luc Picard',
+  citation: 'Star Trek: The Next Generation'
+},
+{
+  quote: 'Humility is no substitute for a good personality.',
+  source: 'Fran Leibowitz',
+  citation: 'Metropolitan Life',
+  year: '1978'
+},
+{
+  quote: 'You have your moments. Not many of them, but you do have them.',
+  source: 'Princess Leia',
+  citation: 'The Empire Strikes Back',
+  year: '1980'
+}
 ]
 
 /***
- * `getRandomQuote` function
+* `getRandomQuote` function
 ***/
 
 /**
- * Returns a random quote from quotes array using randomNum.
- * 
- * @param {number} array - The array of quotes.
- * @return {string} randomQuote - The random Quote.
- */
+* Returns a random quote from quotes array using randomNum.
+* 
+* @param {number} array - The array of quotes.
+* @return {string} randomQuote - The random Quote.
+*/
 
-function getRandomQuote(array) {
-  const randomNum = Math.floor(Math.random() * array.length);
-  const randomQuote = array[randomNum];
-  return randomQuote;
+function getRandomQuote( array ) {
+// create random number
+const randomNum = Math.floor( Math.random() * array.length );
+//Using random number for quote index
+const randomQuote = array[ randomNum ];
+return randomQuote;
 }
 
-console.log(getRandomQuote(quotes));
+
+// check for errors
+// console.log( getRandomQuote( quotes ) );
 /***
- * `printQuote` function
+* `printQuote` function
 ***/
 /**
- * Returns full HTML string displaying a random quote.
- * 
- */
+* Returns full HTML string displaying a random quote.
+* 
+* @returns displayString - The actual Random quote with full available text.
+*/
 
 function printQuote() {
-  let randomQuote = getRandomQuote(quotes);
-  let displayString = `<p class="quote">${randomQuote.quote}</p>
-  <p class="source">${randomQuote.source}`;
-  if (randomQuote.citation) {
-    displayString += `<span class="citation">${randomQuote.citation}</span>`;
-  }  
-  if (randomQuote.year){
-    displayString += `<span class="year">${randomQuote.year}</span>`;
-  }
-  displayString += `</p>`
-  document.getElementById('quote-box').innerHTML = displayString;
-  return displayString;
+let randomQuote = getRandomQuote( quotes );
+let displayString = `<p class="quote">${randomQuote.quote}</p>
+<p class="source">${randomQuote.source}`;
+if ( randomQuote.citation ) {
+  displayString += `<span class="citation">${randomQuote.citation}</span>`;
+}
+if ( randomQuote.year ) {
+  displayString += `<span class="year">${randomQuote.year}</span>`;
+}
+displayString += `</p>`
+document.getElementById( 'quote-box' ).innerHTML = displayString;
+return displayString;
 }
 
 
 
 /***
- * click event listener for the print quote button
- * DO NOT CHANGE THE CODE BELOW!!
+* click event listener for the print quote button
+* DO NOT CHANGE THE CODE BELOW!!
 ***/
 
-document.getElementById('load-quote').addEventListener("click", printQuote, false);
+document.getElementById( 'load-quote' ).addEventListener( "click", printQuote, false );
